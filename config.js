b334765acc75ecf1dfe67a6fe4cabbc67e2d9716
Jacob <73400909+Infinium-dev@users.noreply.github.com>
2020-11-12 15:23:26 +0100

    Initial verison of infinium explorer v0.0.1

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = 'https://node.infinium.space';
+var apiList = ["https://node.infinium.space"];
+var blockTargetInterval = 90;
+var coinUnits = 1000000000000;
+var symbol = 'INF';
+var refreshDelay = 50000;
+var whiteTheme = "css/themes/dark-theme.css";
+var nightTheme = "css/themes/dark-theme.css";
+// pools stats by MainCoins
+var poolsStat = 
+	[
+		[]
+    ];
+var nodesStat = 
+	[
+        []
+    ];
